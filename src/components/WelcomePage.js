32be8457c792a354f86717d3daabcb25f5f0d7f2
Carolina Carrilho <a.carolina.carrilho@gmail.com>
2023-11-04 17:55:46 +0000

    feat: add a logout button

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { Button, CircularProgress } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
+import '../util/welcomePage.css';
 
 function WelcomePage() {
     const [userData, setUserData] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
 
@@ -10,18 +15,34 @@ function WelcomePage() {
             .then((data) => setUserData(data.data));
     }, []);
 
-    return (
-        <div>
-            <h1>Welcome Page</h1>
-            {userData ? (
-                <div>
-                    <p>Welcome {userData.first_name} </p>
+    const handleLogout = () => {
+        localStorage.token = undefined
+        navigate("/login");
+    };
 
-                </div>
+    return (
+        <div className="welcome-page-container">
+            <Button
+                variant="contained"
+                color="secondary"
+                startIcon={<ExitToApp />}
+                onClick={handleLogout}
+                className="logout-button"
+            >
+                Logout
+            </Button>
+            <h1>Welcome Page</h1>
+
+            {userData ? (
+                <div className="welcome-content" >
+                    <p>Welcome {userData.first_name}</p>
+
+                </div >
             ) : (
-                <p>Loading...</p>
-            )}
-        </div>
+                <CircularProgress />
+            )
+            }
+        </div >
     );
 }
 
